73b3dc0dc00ef4eff09dd033b7e7351675d92f55
GSovren <stewart.ngozo69@gmail.com>
2023-11-21 16:24:23 +0200

    version 1.2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,15 +10,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def login():
     return render_template('login.html')
 
-if __name__ == '__main__': 
-    app.run(host='172.16.3.132') 
 
-
-@app.route('/home', methods=['POST', 'GET'])
-def home():
+@app.route('/shed', methods=['POST', 'GET'])
+def shed():
     if request.method == 'POST':
         
         req = request.form
@@ -34,7 +31,23 @@ def home():
 
         return redirect(request.url)
 
+    return render_template('shed.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+@app.route('/home')
+def home():
     return render_template('home.html')
+
+@app.route('/restore')
+def restore():
+    return render_template('restore.html')
 
 # @app.route('/submit', methods=['POST'])
 # def home():
